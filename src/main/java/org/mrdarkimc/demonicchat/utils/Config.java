585fa35fafa37da89c8d6cf6a895c1f80923541c
Daniel <Den_18_1999@mail.ru>
2024-10-15 13:41:39 +0300

    cleanup

--- src/main/java/org/mrdarkimc/demonicchat/utils/Config.java
+++ src/main/java/org/mrdarkimc/demonicchat/utils/Config.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.mrdarkimc.demonicchat.utils;
 
 import org.bukkit.configuration.file.FileConfiguration;
@@ -24,30 +23,3 @@ public class Config {
         return config;
     }
 }
-=======
-package org.mrdarkimc.demonicchat.utils;
-
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.mrdarkimc.demonicchat.DemonicChat;
-
-import java.io.File;
-
-public class Config {
-    static File configFile;
-    static FileConfiguration config;
-    private static void loadConfig() {
-        configFile = new File(DemonicChat.getInstance().getDataFolder(), "config.yml");
-        if (!configFile.exists()) {
-            DemonicChat.getInstance().saveResource("config.yml", false);
-        }
-        config = YamlConfiguration.loadConfiguration(configFile);
-    }
-    public static FileConfiguration getConfig(){
-        if (configFile==null){
-            loadConfig();
-        }
-        return config;
-    }
-}
->>>>>>> d6395a90a5148a8ee11623312d07054556bd3036
